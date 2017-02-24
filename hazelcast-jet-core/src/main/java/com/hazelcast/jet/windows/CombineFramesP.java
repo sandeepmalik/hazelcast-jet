0ba1b5951f03c5d40986d99e8c196349db3395f1
Marko Topolnik <marko@hazelcast.com>
2017-02-24 11:12:19 +0100

    Windowing processors

---
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/windows/CombineFramesP.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.windows;
+
+import com.hazelcast.jet.AbstractProcessor;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import static com.hazelcast.jet.Util.entry;
+
+/**
+ * Javadoc pending.
+ */
+public class CombineFramesP<F, R> extends AbstractProcessor {
+    private final TwoTieredCollector<?, ?, F, R> tc;
+    private final int expectedGroupSize;
+    private final Map<Long, List<F>> seqToFrames = new HashMap<>();
+
+    public CombineFramesP(TwoTieredCollector<?, ?, F, R> tc, int expectedGroupSize) {
+        this.tc = tc;
+        this.expectedGroupSize = expectedGroupSize;
+    }
+
+    @Override
+    protected boolean tryProcess0(@Nonnull Object item) throws Exception {
+        final Entry<Long, F> e = (Entry<Long, F>) item;
+        final Long frameSeq = e.getKey();
+        final F frame = e.getValue();
+        final List<F> frameGroup = seqToFrames.computeIfAbsent(frameSeq, fs -> new ArrayList<>());
+        frameGroup.add(frame);
+        if (frameGroup.size() == expectedGroupSize) {
+            F combined = frameGroup.get(0);
+            for (int i = 1; i < frameGroup.size(); i++) {
+                combined = tc.combiner().apply(combined, frameGroup.get(i));
+            }
+            emit(entry(frameSeq, tc.finisher().apply(combined)));
+            seqToFrames.remove(frameSeq);
+        }
+        return true;
+    }
+}
---
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/windows/GroupByFrameP.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.windows;
+
+import com.hazelcast.jet.AbstractProcessor;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.function.LongUnaryOperator;
+import java.util.function.ToLongFunction;
+
+import static com.hazelcast.jet.Util.entry;
+
+/**
+ * Javadoc pending.
+ */
+public class GroupByFrameP<T, B, F, R> extends AbstractProcessor {
+    private final TwoTieredCollector<T, B, F, R> tc;
+    private final ToLongFunction<? super T> extractTimestampF;
+    private final LongUnaryOperator toFrameSeqF;
+    private final int bucketCount;
+    private final B[] buckets;
+    private long currentFrameSeq;
+
+    public GroupByFrameP(int bucketCount,
+                         ToLongFunction<? super T> extractTimestampF,
+                         LongUnaryOperator toFrameSeqF,
+                         TwoTieredCollector<T, B, F, R> tc
+    ) {
+        this.tc = tc;
+        this.extractTimestampF = extractTimestampF;
+        this.toFrameSeqF = toFrameSeqF;
+        this.bucketCount = bucketCount;
+        this.buckets = (B[]) new Object[bucketCount];
+        Arrays.setAll(buckets, i -> tc.bucketSupplier().get());
+    }
+
+    @Override
+    protected boolean tryProcess0(@Nonnull Object item) {
+        T t = (T) item;
+        final long itemFrameSeq = toFrameSeqF.applyAsLong(extractTimestampF.applyAsLong(t));
+        ensureFrameSeqInitialized(itemFrameSeq);
+        if (itemFrameSeq <= currentFrameSeq - bucketCount) {
+            System.out.println("Late event: " + t);
+            return true;
+        }
+        if (itemFrameSeq > currentFrameSeq) {
+            slideTo(itemFrameSeq);
+        }
+        tc.accumulator().accept(buckets[toBucketIndex(itemFrameSeq)], t);
+        return true;
+    }
+
+    private void slideTo(long itemFrameSeq) {
+        final long bucketCountToEvict = itemFrameSeq - currentFrameSeq;
+        final long oldestFrameSeq = currentFrameSeq - bucketCount + 1;
+        for (long i = 0; i < bucketCountToEvict; i++) {
+            final long frameSeqBeingEvicted = oldestFrameSeq + i;
+            final int bucketIndex = toBucketIndex(frameSeqBeingEvicted);
+            emit(entry(frameSeqBeingEvicted, tc.bucketToFrameTransformer().apply(buckets[bucketIndex])));
+            buckets[bucketIndex] = tc.bucketSupplier().get();
+        }
+        currentFrameSeq = itemFrameSeq;
+    }
+
+    private int toBucketIndex(long tsPeriod) {
+        return (int) (tsPeriod % bucketCount);
+    }
+
+    private void ensureFrameSeqInitialized(long frameSeq) {
+        if (currentFrameSeq == 0) {
+            currentFrameSeq = frameSeq;
+        }
+    }
+}
