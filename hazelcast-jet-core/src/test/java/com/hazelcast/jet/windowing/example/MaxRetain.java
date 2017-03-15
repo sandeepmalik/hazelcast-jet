eadf8f483a5b838d09df54818b8ded992d1cd850
Can Gencer <cgencer@gmail.com>
2017-03-15 15:49:13 +0300

    Add MaxRetain class

---
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/windowing/example/MaxRetain.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.windowing.example;
+
+import java.util.Arrays;
+
+public class MaxRetain {
+
+    private long[] seqs;
+    private int head = 0;
+    private int tail = 1;
+    private long interval;
+    private long nextSlotAt;
+
+    MaxRetain(int count, long maxRetain) {
+        seqs = new long[count];
+        Arrays.fill(seqs, Long.MIN_VALUE);
+        interval = maxRetain / count;
+    }
+
+    public void init(long now) {
+        nextSlotAt = now + interval;
+    }
+
+    long tick(long now, long topSeq) {
+        long val = seqs[tail];
+        for (; now >= nextSlotAt; nextSlotAt += interval) {
+            val = slide();
+        }
+        seqs[head] = topSeq;
+        return val;
+    }
+
+    private long slide() {
+        // advance tail and keep value of current tail
+        long val = seqs[tail];
+        tail = advance(tail);
+
+        // advance head and copy value to new head
+        long currHead = seqs[head];
+        head = advance(head);
+        seqs[head] = currHead;
+
+        return val;
+    }
+
+    private int advance(int index) {
+        if (++index == seqs.length) {
+            return 0;
+        }
+        return index;
+    }
+}
---
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/windowing/example/MaxRetainTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.windowing.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaxRetainTest {
+
+    private MaxRetain retain;
+
+    @Before
+    public void setup() {
+        retain = new MaxRetain(4, 4);
+        retain.init(0);
+    }
+
+    @Test
+    public void when_clockIncreasingByOne() {
+        assertEquals(Long.MIN_VALUE, tick(1,1));
+        assertEquals(Long.MIN_VALUE, tick(2,2));
+        assertEquals(Long.MIN_VALUE, tick(3,3));
+        assertEquals(Long.MIN_VALUE, tick(4,4));
+        assertEquals(1,  tick(5,5));
+        assertEquals(2, tick(6,6));
+        assertEquals(3, tick(7,7));
+        assertEquals(4, tick(8,8));
+    }
+
+    @Test
+    public void when_clockIncreasingByTwo() {
+        assertEquals(Long.MIN_VALUE, tick(1,1));
+        assertEquals(Long.MIN_VALUE, tick(3,2));
+        assertEquals(1, tick(5,3));
+        assertEquals(2, tick(7,4));
+        assertEquals(3,  tick(9,5));
+    }
+
+    @Test
+    public void when_clockIncreasingByThree() {
+        assertEquals(Long.MIN_VALUE, tick(1,1));
+        assertEquals(Long.MIN_VALUE, tick(4,2));
+        assertEquals(1, tick(7,3));
+        assertEquals(2, tick(10,4));
+    }
+
+    @Test
+    public void when_clockIncreasingByFour() {
+        assertEquals(Long.MIN_VALUE, tick(1,1));
+        assertEquals(1, tick(5,2));
+        assertEquals(2, tick(9,3));
+        assertEquals(3, tick(13,4));
+        assertEquals(4,  tick(17,5));
+    }
+
+    @Test
+    public void when_clockIncreasingByFive() {
+        assertEquals(Long.MIN_VALUE, tick(1,1));
+        assertEquals(1, tick(6,2));
+        assertEquals(2, tick(11,3));
+        assertEquals(3, tick(16,4));
+    }
+
+    private long tick(long now, long topSeq) {
+        return retain.tick(now, topSeq);
+    }
+}
