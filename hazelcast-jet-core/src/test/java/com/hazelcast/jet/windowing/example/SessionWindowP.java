8b68e758e920cdc175fb39c6a0f603ef7be1fdd1
Marko Topolnik <marko@hazelcast.com>
2017-03-23 11:44:50 +0100

    Mild refactoring in SessionWindowP

--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/windowing/example/SessionWindowP.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/windowing/example/SessionWindowP.java
@@ -27,87 +27,87 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.BiConsumer;
 
 public class SessionWindowP<T, K, A> extends StreamingProcessorBase {
 
     private final ToLongFunction<? super T> extractEventSeqF;
     private final Function<? super T, K> extractKeyF;
-    private final long sessionTimeout;
-    private final DistributedCollector<T, A, ?> collector;
+    private final BiConsumer<? super A, ? super T> accumulateF;
+    private final long maxSeqGap;
     private final long emissionInterval;
 
-    private Map<K, Session> map = new HashMap<>();
-    private long now;
+    private Map<K, Session> keyToSession = new HashMap<>();
+    private long lastPunc;
     private long nextEmission;
 
-    private Iterator<Entry<K, Session>> emissionIterator;
+    private Iterator<Entry<K, Session>> sessionIterator;
 
-    public SessionWindowP(ToLongFunction<? super T> extractEventSeqF, Function<? super T, K> extractKeyF, long sessionTimeout,
-            DistributedCollector<T, A, ?> collector, long emissionInterval) {
+    public SessionWindowP(ToLongFunction<? super T> extractEventSeqF, Function<? super T, K> extractKeyF,
+                          long maxSeqGap, DistributedCollector<T, A, ?> collector, long emissionInterval
+    ) {
         this.extractEventSeqF = extractEventSeqF;
         this.extractKeyF = extractKeyF;
-        this.sessionTimeout = sessionTimeout;
-        this.collector = collector;
+        this.accumulateF = collector.accumulator();
+        this.maxSeqGap = maxSeqGap;
         this.emissionInterval = emissionInterval;
     }
 
     @Override
-    protected boolean tryProcess(int ordinal, @Nonnull Object item_) throws Exception {
-        //noinspection unchecked
-        T item = (T) item_;
-        K key = extractKeyF.apply(item);
+    protected boolean tryProcess0(@Nonnull Object item) {
+        T t = (T) item;
+        K key = extractKeyF.apply(t);
         // note that we might accumulate to windows, that should be already closed, but are not
         // due to full outbox. I don't think this is a concern...
-        map.computeIfAbsent(key, k -> new Session())
-                .merge(item);
+        keyToSession.computeIfAbsent(key, k -> new Session())
+                    .merge(t);
 
         return true;
     }
 
     @Override
-    protected boolean tryProcessPunc(int ordinal, @Nonnull Punctuation punc) {
+    protected boolean tryProcessPunc0(@Nonnull Punctuation punc) {
         // now is the punc we received. This way we account for input stream lag
-        now = punc.seq();
+        lastPunc = punc.seq();
         return true;
     }
 
     @Override
     public void process() {
-        if (emissionIterator == null && nextEmission < now) {
-            emissionIterator = map.entrySet().iterator();
+        if (sessionIterator == null) {
+            if (lastPunc <= nextEmission) {
+                return;
+            }
+            sessionIterator = keyToSession.entrySet().iterator();
         }
 
-        if (emissionIterator != null) {
-            // close windows that timed out
-            while (emissionIterator.hasNext()) {
-                if (getOutbox().hasReachedLimit()) {
-                    // If outbox is full, just return without updating nextEmission.
-                    // This way, we'll continue after we're called again
-                    return;
-                }
-
-                Entry<K, Session> entry = emissionIterator.next();
-                if (entry.getValue().timeoutAt <= now) {
-                    emissionIterator.remove();
-                    // frame seq is the end of session
-                    emit(new Frame<>(entry.getValue().timeoutAt, entry.getKey(), entry.getValue().accumulator));
-                }
+        // close windows that timed out
+        while (sessionIterator.hasNext()) {
+            if (getOutbox().hasReachedLimit()) {
+                return;
             }
 
-            // we are done emitting this iterator
-            emissionIterator = null;
-            nextEmission = now + emissionInterval;
+            Entry<K, Session> entry = sessionIterator.next();
+            if (entry.getValue().closeAtPunc <= lastPunc) {
+                sessionIterator.remove();
+                // frame seq is the end of session
+                emit(new Frame<>(entry.getValue().closeAtPunc, entry.getKey(), entry.getValue().acc));
+            }
         }
+
+        // we are done emitting this iterator
+        sessionIterator = null;
+        nextEmission = lastPunc + emissionInterval;
     }
 
     private final class Session {
-        long timeoutAt;
-        A accumulator;
+        long closeAtPunc;
+        A acc;
 
         void merge(T item) {
             long eventSeq = extractEventSeqF.applyAsLong(item);
-            timeoutAt = Math.max(timeoutAt, eventSeq + sessionTimeout);
-            collector.accumulator().accept(accumulator, item);
+            closeAtPunc = Math.max(closeAtPunc, eventSeq + maxSeqGap);
+            accumulateF.accept(acc, item);
         }
     }
 }
