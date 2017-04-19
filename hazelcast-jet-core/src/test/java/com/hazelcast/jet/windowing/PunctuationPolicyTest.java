ed8f5b4c855e1f414f09cb0d9fc248f7defa3595
Marko Topolnik <marko@hazelcast.com>
2017-04-19 14:15:27 +0200

    Rename ThrottlingPunctuationTest

--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/windowing/PunctuationPolicyTest.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/windowing/PunctuationPolicyTest.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ThrottlingPunctuationTest {
+public class PunctuationPolicyTest {
 
     private static final int MIN_STEP = 2;
     private long punc;
